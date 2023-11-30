5ecea63b6fa47cb051854d9efac7c068d83967d0
Andyana Lilmuttaqina Mafaza <andyanamafaza@gmail.com>
2023-11-30 17:04:50 +0700

    minor changes

--- script.py
+++ script.py
@@ -407,14 +407,12 @@ def generator(height, width):
     print("Generating board...")
     max_attempts = 10
 
-    testing_board = generate_crossword_board(height, width)
-    
-    #if board height and width is less than 3, then give a board with all -'s
+    # #if board height and width is less than 3, then give a board with all -'s
     if height <= 3 and width <= 3:
-        for i in range(height):
-            for j in range(width):
-                testing_board[i][j] = '-'
-        return testing_board
+        board = [['-' for _ in range(width)] for _ in range(height)]
+        return board
+    
+    testing_board = generate_crossword_board(height, width)
 
     while True:
         word_length = random.randint(1, min(height, width))
