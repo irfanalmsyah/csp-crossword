0f27da1f845b365ab119a51b54d9ab911720211c
Irfan Alamsyah <88070126+irfanalmsyah@users.noreply.github.com>
2023-12-13 22:19:48 +0700

    Refactor generator.py: Improve word placement algorithm and calculate board occupancy

--- generator.py
+++ generator.py
@@ -1,7 +1,7 @@
 import random
 import time
 import copy
-from solver import *
+from solver import create_variables, create_arc, arc_consistency_3, select_unassigned_variable, satisfy_constraint, read_file
 
 TIME_LIMIT_SECONDS = 1
 
@@ -123,8 +123,19 @@ def add_word_to_board(board, length, position):
     direction = "horizontal" if position % 2 == 0 else "vertical"
 
     for _ in range(max_attempts):
-        i, j = random.choice([(x, y) for x in range(len(board)) for y in range(len(board[0]) - length + 1)]
-                             ) if direction == "horizontal" else random.choice([(x, y) for x in range(len(board) - length + 1) for y in range(len(board[0]))])
+        if direction == "horizontal":
+            choices = [
+                (x, y)
+                for x in range(len(board))
+                for y in range(len(board[0]) - length + 1)
+            ]
+        else:
+            choices = [
+                (x, y)
+                for x in range(len(board) - length + 1)
+                for y in range(len(board[0]))
+            ]
+        i, j = random.choice(choices)
 
         if is_valid_placement(board, length, i, j, direction):
             if direction == "horizontal":
@@ -142,44 +153,66 @@ def save_board(board):
                 file.write("\n")
 
 
+def occupancy(board):
+    count = 0
+    for row in board:
+        for col in row:
+            if col == "-":
+                count += 1
+    return count / (len(board) * len(board[0]))
+
+
+def most_common_char_length(words):
+    char_length = {}
+
+    for word in words:
+        length = len(word)
+        char_length[length] = char_length.get(length, 0) + 1
+
+    return sorted(char_length, key=char_length.get, reverse=True)
+
+
 if __name__ == "__main__":
     height = 15
     width = 15
     max_attempts = 10
-
+    words = read_file("words.txt").splitlines()
+    mylist = most_common_char_length(words)
+    total_elements = len(mylist)
+    probabilities = [1 / (i + 1) for i in range(total_elements)]
+    sum_probabilities = sum(probabilities)
+    normalized_probabilities = [
+        prob / sum_probabilities for prob in probabilities
+    ]
     time_start = time.time()
-
     testing_board = generate_crossword_board(height, width)
-    
-    #if board height and width is less than 3, then give a board with all -'s
+
     if height <= 3 and width <= 3:
         for i in range(height):
             for j in range(width):
                 testing_board[i][j] = '-'
         save_board(testing_board)
         exit()
-        
 
     while True:
-        word_length = random.randint(1, min(height, width))
+        word_length = random.choices(
+            mylist, weights=normalized_probabilities, k=1
+        )[0]
         choice = random.randint(1, 2)  # vertical or horizontal
-        final_board = copy.deepcopy(testing_board)
+        backup_board = copy.deepcopy(testing_board)
         add_word_to_board(testing_board, word_length, choice)
 
-        # If the board is not solveable, revert back to the old board
-        if not is_solveable(testing_board, read_file("words.txt").splitlines()):
-            # try adding the word for 10 times
-            for _ in range(max_attempts):
-                testing_board = copy.deepcopy(final_board)
-                add_word_to_board(testing_board, word_length, choice)
-                if is_solveable(testing_board, read_file("words.txt").splitlines()):
-                    break
-            else:
-                # Save the board and break the loop if not solveable after 5 attempts
-                save_board(final_board)
-                break
+        if is_solveable(testing_board, words):
+            final_board = copy.deepcopy(testing_board)
+            print(occupancy(final_board))
+        else:
+            testing_board = copy.deepcopy(backup_board)
 
+        if occupancy(final_board) > 0.3:
+            save_board(final_board)
+            break
 
     print("Time taken: {} seconds".format(elapsed_time(time_start)))
     print("Final board:")
     print_crossword_board(final_board)
+    print("Occupancy: {}%".format(occupancy(final_board) * 100))
